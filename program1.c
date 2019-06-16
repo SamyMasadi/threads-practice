4acb51a5f2f0c034934afecfa4c84333e44097f6
SamyMasadi <samymasadi@gmail.com>
2019-06-15 20:18:04 -0400

    Change Printing and Output
    - Move printing out of the threaded function.
    - Each thread will save its local min/max findings.
    - Saved thread details are printed near the end of program execution.
    - Threaded search time measurement thus no longer includes printing, and
    only measures multithreaded search performance.

--- program1.c
+++ program1.c
@@ -35,9 +35,15 @@ unsigned int randSeed;
 pthread_mutex_t lockA;
 pthread_mutex_t lockB;
 
+struct ThreadDetails
+{
+    int id;
+    int min;
+    int max;
+};
 
 // threaded function
-void* run(void* args)
+void *run(void *args)
 {
     // critical section for thread to select its array segment bounds
     pthread_mutex_lock( &lockA );
@@ -57,36 +63,36 @@ void* run(void* args)
     pthread_mutex_unlock( &lockA );
     // end critical section
 
+    // cast type back to ThreadDetails to access variables
+    struct ThreadDetails *details = (struct ThreadDetails *)args;
+
     // init local min/max
-    int smallestL = arr[start];
-    int largestL = arr[start];
+    details->min = arr[start];
+    details->max = arr[start];
 
     // find local min/max
-    for (int i = start; i < end; i++)
+    for (int i = start; i < end; ++i)
     {
-        if (arr[i] < smallestL)
+        if (arr[i] < details->min)
         {
-            smallestL = arr[i];
+            details->min = arr[i];
         }
 
-        if (arr[i] > largestL)
+        if (arr[i] > details->max)
         {
-            largestL = arr[i];
+            details->max = arr[i];
         }
     }
-
-    printf("Thread local max: %d\n", largestL);
-    printf("Thread local min: %d\n", smallestL);
 
     // critical section to update global max/min
     pthread_mutex_lock( &lockB );
-    if (smallestL < smallest)
+    if (details->min < smallest)
     {
-        smallest = smallestL;
+        smallest = details->min;
     }
-    if (largestL > largest)
+    if (details->max > largest)
     {
-        largest = largestL;
+        largest = details->max;
     }
     pthread_mutex_unlock( &lockB );
     // end critical section
@@ -98,7 +104,7 @@ void* run(void* args)
 // pid is the seed
 void fill_arr()
 {
-    for (int i = 0; i < ARR_LEN; i++)
+    for (int i = 0; i < ARR_LEN; ++i)
     {
         arr[i] = rand_r(&randSeed);
     }
@@ -114,8 +120,11 @@ int main()
     printf("Enter number of threads: ");
     num_threads = getchar();
 
-    // start clock after user input
+    // start program clock after user input
     clock_t start_time = clock();
+
+    // just a space for readability
+    printf("\n");
 
     // determine intended int value
     num_threads = num_threads - '0';
@@ -135,9 +144,13 @@ int main()
 
     // declare an array of threads
     pthread_t threads[num_threads];
+
+    // declare a corresponding array of thread details
+    struct ThreadDetails thread_details[num_threads];
+
     // init locks
-    pthread_mutex_init( &lockA, NULL);
-    pthread_mutex_init( &lockB, NULL);
+    pthread_mutex_init(&lockA, NULL);
+    pthread_mutex_init(&lockB, NULL);
 
     // populate the array    
     fill_arr();
@@ -146,25 +159,42 @@ int main()
     smallest = arr[0];
     largest = arr[0];
 
+    // start search clock
+    clock_t start_search = clock();
+
     // start the threads
-    for (int i = 0; i < num_threads; i++)
+    for (int i = 0; i < num_threads; ++i)
     {
-        pthread_create(&threads[i], NULL, run, NULL);
+        thread_details[i].id = i;
+        pthread_create(&threads[i], NULL, run, &thread_details[i]);
     }
 
     // wait for all threads to complete
-    for (int i = 0; i < num_threads; i++)
+    for (int i = 0; i < num_threads; ++i)
     {
         pthread_join(threads[i], NULL);
     }
 
-    printf("Global min: %d\n", smallest);
-    printf("Global max: %d\n", largest);
+    // stop search clock
+    clock_t end_search = clock();
+    double search_time = (double)(end_search = start_search) / CLOCKS_PER_SEC;
 
-    // stop clock
+    // print out local details, global details, and elapsed times
+    for (int i = 0; i < num_threads; ++i)
+    {
+        printf("Thread %d local min: %d\n", thread_details[i].id, thread_details[i].min);
+        printf("Thread %d local max: %d\n", thread_details[i].id, thread_details[i].max);
+    }
+
+    printf("\nGlobal min: %d\n", smallest);
+    printf("Global max: %d\n\n", largest);
+
+    // stop program clock
     clock_t end_time = clock();
     double time_used = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-    printf("Time taken: %f seconds.\n", time_used);
+    
+    printf("Search time: %f seconds\n", search_time);
+    printf("Program time: %f seconds\n", time_used);
 
 }
 	
